b284a2ca96b376aba71b8dd27eec86419ed20abb
Michael Nordheimer <nordheimerm@gmail.com>
2020-03-02 13:08:02 +0300

    + updated admin index

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,51 +1,39 @@
 <?php
 
+use yii\helpers\Url;
+
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Управление сайтом';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Панель управления сайтом</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead text-left">В горизонтальном меню размещены все разделы сайта:</p>
+        <ul class="list-unstyled text-left">
+            <li><span class="glyphicon glyphicon-hdd"></span> Файловый менеджер</li>
+            <li><span class="glyphicon glyphicon-user"></span> Управление пользователями</li>
+            <li><span class="glyphicon glyphicon-inbox"></span> Менеджер категорий</li>
+            <li><span class="glyphicon glyphicon-pencil"></span> Менеджер материалов</li>
+            <li><span class="glyphicon glyphicon-tags"></span> Управление тегами</li>
+            <li><span class="glyphicon glyphicon-ok"></span> Менеджер опросов</li>
+            <li><span class="glyphicon glyphicon-education"></span> Управление тестами</li>
+            <li><span class="glyphicon glyphicon-question-sign"></span> Менеджер тестовых вопросов</li>
+        </ul>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2><span class="glyphicon glyphicon-pencil"></span> Менеджер материалов</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Система управления контентон с поддержкой технологии Google AMP</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <p><a class="btn btn-default" href="<?= Url::toRoute(['/cms/cms-article/index']) ?>">Перейти &raquo;</a></p>
             </div>
         </div>
 
